c99056bfaa4ee60b2d3d587b7d696c4392f8656f
Scott Morales <morales.scott@gmail.com>
2019-06-01 00:43:51 -0500

    add button opens addPart scene

--- src/View_Controller/MainScreenController.java
+++ src/View_Controller/MainScreenController.java
@@ -5,15 +5,23 @@
  */
 package View_Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -78,7 +86,14 @@ public class MainScreenController implements Initializable {
     }
 
     @FXML
-    private void addHandler(ActionEvent event) {
+    private void addHandler(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addPart.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("ABC");
+        stage.setScene(new Scene(root1));  
+        stage.show();
     }
 
     @FXML
